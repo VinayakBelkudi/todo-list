e5e9bbc93f8272d09bff83ded077e28b7fdbab30
VinayakBelkudi <vinayakbelkudi@gmail.com>
2019-07-20 03:08:17 +0530

    i added bend file

---
+++ bend/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {environment} from '../environments/environment';
+import { TodoComponent } from './todo/todo.component';
+
+import { Observable } from 'rxjs';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ bend/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB-m5vhIHluL50-ViiBRNsSHddhDMpt2Qo',
+    authDomain: 'todo-52c71.firebaseapp.com',
+    databaseURL: 'https://todo-52c71.firebaseio.com',
+    projectId: 'todo-52c71',
+    storageBucket: 'todo-52c71.appspot.com',
+    messagingSenderId: '222105315792',
+    appId: '1:222105315792:web:4caeab94d07e0b4d'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
